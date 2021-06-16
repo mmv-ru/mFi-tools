ba6460c77167aea1254d4f175e09b9bffe35ed44
MMV <mmv.rus@gmail.com>
2021-06-16 05:07:01 +0300

    Multiple fixes. Get all best
    import $stat/load and port/pf from dncodes/mpower-tools
    import $mac from Chris-42/mpower-tools
    fix $localip detection
    support sensors of same type on multiple ports on mPort
    Fix Homie property and make full Homie 3.0.0 compliant
    Add RTD sensor support

--- mqtt/client/mqpub-static.sh
+++ mqtt/client/mqpub-static.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+model_lookup() {
+    awk -F ";" "/$1/ { print \$$2 }" $BIN_PATH/client/model.cfg
+}
+
 # homie spec (incomplete)
 $PUBBIN -h $mqtthost $auth -t $topic/\$homie -m "3.0.0" -r
+$PUBBIN -h $mqtthost $auth -t $topic/\$state -m "init" -r
 $PUBBIN -h $mqtthost $auth -t $topic/\$name -m "$devicename" -r
 $PUBBIN -h $mqtthost $auth -t $topic/\$fw/version -m "$version" -r
 
@@ -9,8 +15,16 @@ export mFiType=`cat /etc/board.inc | grep board_name | sed -e 's/.*="\(.*\)";/\1
 
 $PUBBIN -h $mqtthost $auth -t $topic/\$fw/name -m "mFi MQTT" -r
 
-IPADDR=`ifconfig ath0 | grep 'inet addr' | cut -d ':' -f 2 | awk '{ print $1 }'`
+for IFNAME in ath0 eth0 eth1 wifi0
+do
+    IPADDR=`ifconfig $IFNAME | grep 'inet addr' | cut -d ':' -f 2 | awk '{ print $1 }'`
+    if [ "$IPADDR" != "" ]; then break; fi
+done
 $PUBBIN -h $mqtthost $auth -t $topic/\$localip -m "$IPADDR" -r
+
+MACADDR=`ifconfig $IFNAME | grep 'HWaddr' | awk '{print $NF}'`
+$PUBBIN -h $mqtthost -t $topic/\$mac -m "$MACADDR" -r
+
 
 NODES=`seq $PORTS | sed 's/\([0-9]\)/port\1/' |  tr '\n' , | sed 's/.$//'`
 $PUBBIN -h $mqtthost $auth -t $topic/\$nodes -m "$NODES" -r
@@ -18,78 +32,73 @@ $PUBBIN -h $mqtthost $auth -t $topic/\$nodes -m "$NODES" -r
 UPTIME=`awk '{print $1}' /proc/uptime`
 $PUBBIN -h $mqtthost $auth -t $topic/\$stats/uptime -m "$UPTIME" -r
 
-properties=relay
 
-if [ $energy -eq 1 ]
+if [ "$mFiType" != "mPower" ] && [ "$mFiType" != "mPower Mini" ] && [ "$mFiType" != "mPower Pro" ]
 then
-    properties=$properties,energy
-fi
-
-if [ $power -eq 1 ]
-then
-    properties=$properties,power
-fi
-
-if [ $voltage -eq 1 ]
-then
-    properties=$properties,voltage
-fi
-
-if [ $current -eq 1 ]
-then
-    properties=$properties,current
-fi
-
-if [ $lock -eq 1 ]
-then
-    properties=$properties,lock
-fi
-
-if [ $mFiTHS -eq 1 ]
-then
-    properties=$properties,mFiTHS
-fi
-
-if [ $mFiCS -eq 1 ]
-then
-    properties=$properties,mFiCS
-fi
-
-if [ $mFiMSW -eq 1 ]
-then
-    properties=$properties,mFiMSW
-fi
-
-if [ $mFiMSC -eq 1 ]
-then
-    properties=$properties,mFiMSC
-fi
-
-if [ $mFiDS -eq 1 ]
-then
-    properties=$properties,mFiDS
-fi
-
-
-if [ "$mFiType" == "mPower" ] || [ "$mFiType" == "mPower Mini" ] || [ "$mFiType" == "mPower Pro" ]
-then
-
-# node infos
-for i in $(seq $PORTS)
-do
-    $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$name -m "Port $i" -r
-    $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$type -m "power switch" -r
-    $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$properties -m "$properties" -r
-    $PUBBIN -h $mqtthost $auth -t $topic/port$i/relay/\$settable -m "true" -r
-done
-
-if [ $lock -eq 1 ]
-then
+    # node infos
     for i in $(seq $PORTS)
     do
-        $PUBBIN -h $mqtthost $auth -t $topic/port$i/lock/\$settable -m "true" -r
+        portname="port$i"
+        eval portrole="\$$portname"
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$name -m "Port $i" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$type -m "$portrole" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$properties -m "$(model_lookup $portrole 2)" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/$(model_lookup $portrole 2)/\$name -m "$(model_lookup $portrole 3)" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/$(model_lookup $portrole 2)/\$settable -m "false" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/$(model_lookup $portrole 2)/\$unit -m "$(model_lookup $portrole 4)" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/$(model_lookup $portrole 2)/\$datatype -m "$(model_lookup $portrole 5)" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/$(model_lookup $portrole 2)/\$format -m "$(model_lookup $portrole 6)" -r
     done
 
 fi
 
+if [ "$mFiType" == "mPower" ] || [ "$mFiType" == "mPower Mini" ] || [ "$mFiType" == "mPower Pro" ]
+then
+    properties=relay
+
+    if [ $energy -eq 1 ]
+    then
+        properties=$properties,energy
+    fi
+
+    if [ $power -eq 1 ]
+    then
+        properties=$properties,power
+    fi
+
+    if [ $voltage -eq 1 ]
+    then
+        properties=$properties,voltage
+    fi
+
+    if [ $current -eq 1 ]
+    then
+        properties=$properties,current
+    fi
+
+    if [ $lock -eq 1 ]
+    then
+        properties=$properties,lock
+    fi
+
+    # node infos
+    for i in $(seq $PORTS)
+    do
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$name -m "Port $i" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$type -m "power switch" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/\$properties -m "$properties" -r
+        $PUBBIN -h $mqtthost $auth -t $topic/port$i/relay/\$settable -m "true" -r
+    done
+
+    if [ $lock -eq 1 ]
+    then
+        for i in $(seq $PORTS)
+        do
+            $PUBBIN -h $mqtthost $auth -t $topic/port$i/lock/\$settable -m "true" -r
+        done
+
+    fi
+
 fi
+
+$PUBBIN -h $mqtthost -t $topic/\$state -m "ready" -r --will-topic $topic/\$state --will-payload lost --will-qos 1
--- mqtt/client/mqpub.sh
+++ mqtt/client/mqpub.sh
@@ -29,6 +29,8 @@ log "Publishing to $mqtthost with topic $topic"
 REFRESHCOUNTER=$refresh
 FASTUPDATE=0
 
+SLOWUPDATECOUNTER=0
+SLOWUPDATENUMBER=6
 
 export relay=$relay
 export power=$power
@@ -45,9 +47,9 @@ export mFiDS=$mFiDS
 $BIN_PATH/client/mqpub-static.sh
 while sleep 1;
 do
-        # refresh logic: either we need fast updates, or we count down until it's time
-        TMPFASTUPDATE=`cat $tmpfile`
-        #echo "TMPFILE = " $TMPFASTUPDATE
+    # refresh logic: either we need fast updates, or we count down until it's time
+    TMPFASTUPDATE=`cat $tmpfile`
+    #echo "TMPFILE = " $TMPFASTUPDATE
     if [ -n "${TMPFASTUPDATE}" ]
         then
                 FASTUPDATE=$TMPFASTUPDATE
@@ -71,147 +73,196 @@ do
                 fi
         fi
 
-if [ "$mFiType" != "mPower" ] && [ "$mFiType" != "mPower Mini" ] && [ "$mFiType" != "mPower Pro" ]
-then
-
-    log "Gathering mPort values."
-    if [ $mFiTHS -eq 1 ] && [ "$port1" == "mFiTHS" ] || [ "$port2" == "mFiTHS" ]
+    if [ "$mFiType" != "mPower" ] && [ "$mFiType" != "mPower Mini" ] && [ "$mFiType" != "mPower Pro" ]
     then
-        #temperature
-        if [ "$port1" == "mFiTHS" ];then
+
+        log "Gathering mPort values."
+
+        # port1
+        if [ $mFiTHS -eq 1 ] && [ "$port1" == "mFiTHS" ]
+        then
+            # temperature
             mFiTHS_val=`cat /proc/analog/value1`
-            $PUBBIN -h $mqtthost $auth -t $topic/port1/mFiTHS -m "$mFiTHS_val" -r
-        elif [ "$port2" == "mFiTHS" ];then
-            mFiTHS_val=`cat /proc/analog/value2`
-            $PUBBIN -h $mqtthost $auth -t $topic/port2/mFiTHS -m "$mFiTHS_val" -r
+            $PUBBIN -h $mqtthost $auth -t $topic/port1/temperature -m "$mFiTHS_val" -r
         fi
-    fi
-
-    if [ $mFiCS -eq 1 ] && [ "$port1" == "mFiCS" ] || [ "$port2" == "mFiCS" ]
-    then
-        #pinca corrente
-        if [ "$port1" == "mFiCS" ];then
+        if [ $mFiCS -eq 1 ]  && [ "$port1" == "mFiCS" ]
+        then
+            #current
             mFiCS_val=`cat /proc/analog/rms1`
-            $PUBBIN -h $mqtthost $auth -t $topic/port1/mFiCS -m "$mFiCS_val" -r
-        elif [ "$port2" == "mFiCS" ];then
-            mFiCS_val=`cat /proc/analog/rms2`
-            $PUBBIN -h $mqtthost $auth -t $topic/port2/mFiCS -m "$mFiCS_val" -r
+            $PUBBIN -h $mqtthost $auth -t $topic/port1/current -m "$mFiCS_val" -r
         fi
-    fi
-
-    if [ $mFiMSW -eq 1 ] && [ "$port1" == "mFiMSW" ] || [ "$port2" == "mFiMSW" ]
-    then
-        #sensor movimento
-        if [ "$port1" == "mFiMSW" ];then
+        if [ $mFiMSW -eq 1 ] && [ "$port1" == "mFiMSW" ]
+        then
+            #sensor movimento
             mFiMSW_val=`cat /dev/input21`
-            $PUBBIN -h $mqtthost $auth -t $topic/port1/mFiMSW -m "$mFiMSW_val" -r
-        elif [ "$port2" == "mFiMSW" ];then
-            mFiMSW_val=`cat /dev/input22`
-            $PUBBIN -h $mqtthost $auth -t $topic/port2/mFiMSW -m "$mFiMSW_val" -r
+            $PUBBIN -h $mqtthost $auth -t $topic/port1/motion -m "$mFiMSW_val" -r
         fi
-    fi
-
-    if [ $mFiMSC -eq 1 ] && [ "$port1" == "mFiMSC" ] || [ "$port2" == "mFiMSC" ]
-    then
-        #sensor movimento
-        if [ "$port1" == "mFiMSC" ];then
+        if [ $mFiMSC -eq 1 ] && [ "$port1" == "mFiMSC" ]
+        then
+            #sensor movimento
             mFiMSC_val=`cat /dev/input21`
-            $PUBBIN -h $mqtthost $auth -t $topic/port1/mFiMSC -m "$mFiMSC_val" -r
-        elif [ "$port2" == "mFiMSC" ];then
+            $PUBBIN -h $mqtthost $auth -t $topic/port1/motion -m "$mFiMSC_val" -r
+        fi
+        if [ $mFiDS -eq 1 ]  && [ "$port1" == "mFiDS" ] || [ "$port2" == "mFiDS" ] || [ "$port3" == "mFiDS" ]
+        then
+            #sensor abertura porta
+            mFiDS_val=`cat /dev/input11`
+            $PUBBIN -h $mqtthost $auth -t $topic/port1/door -m "$mFiDS_val" -r
+        fi
+
+        # port2
+        if [ $mFiTHS -eq 1 ] && [ "$port2" == "mFiTHS" ]
+        then
+            #temperature
+            mFiTHS_val=`cat /proc/analog/value2`
+            $PUBBIN -h $mqtthost $auth -t $topic/port2/temperature -m "$mFiTHS_val" -r
+        fi
+        if [ $mFiCS -eq 1 ] && [ "$port2" == "mFiCS" ]
+        then
+            #pinca corrente
+            mFiCS_val=`cat /proc/analog/rms2`
+            $PUBBIN -h $mqtthost $auth -t $topic/port2/current -m "$mFiCS_val" -r
+        fi
+
+        if [ $mFiMSW -eq 1 ] && [ "$port2" == "mFiMSW" ]
+        then
+            #sensor movimento
+            mFiMSW_val=`cat /dev/input22`
+            $PUBBIN -h $mqtthost $auth -t $topic/port2/motion -m "$mFiMSW_val" -r
+        fi
+
+        if [ $mFiMSC -eq 1 ] && [ "$port2" == "mFiMSC" ]
+        then
+            #sensor movimento
             mFiMSC_val=`cat /dev/input22`
-            $PUBBIN -h $mqtthost $auth -t $topic/port2/mFiMSC -m "$mFiMSC_val" -r
+            $PUBBIN -h $mqtthost $auth -t $topic/port2/motion -m "$mFiMSC_val" -r
+        fi
+
+        if [ $mFiDS -eq 1 ] && [ "$port2" == "mFiDS" ]
+        then
+            #sensor abertura porta
+            mFiDS_val=`cat /dev/input12`
+            $PUBBIN -h $mqtthost $auth -t $topic/port2/door -m "$mFiDS_val" -r
+        fi
+
+        # port3
+        if [ $mFiDS -eq 1 ] && [ "$port3" == "mFiDS" ]
+        then
+            #sensor abertura porta
+            mFiDS_val=`cat /dev/input13`
+            $PUBBIN -h $mqtthost $auth -t $topic/port3/door -m "$mFiDS_val" -r
+        fi
+
+        if [ $RTD -eq 1 ] && [ "$port3" == "RTD" ]
+        then
+            #sensor abertura porta
+            RTD_val=`cat /proc/analog/value4`
+            $PUBBIN -h $mqtthost $auth -t $topic/port3/temperature -m "$RTD_val" -r
         fi
     fi
 
-    if [ $mFiDS -eq 1 ] && [ "$port1" == "mFiDS" ] || [ "$port2" == "mFiDS" ] || [ "$port3" == "mFiDS" ]
+    if [ "$mFiType" == "mPower" ] || [ "$mFiType" == "mPower Mini" ] || [ "$mFiType" == "mPower Pro" ]
     then
-        #sensor abertura porta
-        if [ "$port1" == "mFiDS" ];then
-              mFiDS_val=`cat /dev/input11`
-              $PUBBIN -h $mqtthost $auth -t $topic/port1/mFiDS -m "$mFiDS_val" -r
-        elif [ "$port2" == "mFiDS" ];then
-              mFiDS_val=`cat /dev/input12`
-              $PUBBIN -h $mqtthost $auth -t $topic/port2/mFiDS -m "$mFiDS_val" -r
-        elif [ "$port3" == "mFiDS" ];then
-              mFiDS_val=`cat /dev/input13`
-              $PUBBIN -h $mqtthost $auth -t $topic/port3/mFiDS -m "$mFiDS_val" -r
+
+        log "Gathering mPower values."
+        if [ $relay -eq 1 ]
+        then
+            # relay state
+            for i in $(seq $PORTS)
+            do
+                relay_val=`cat /proc/power/relay$((i))`
+                if [ $relay_val -ne 1 ]
+                then
+                  relay_val=0
+                fi
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/relay -m "$relay_val" -r
+            done
+        fi
+
+        if [ $power -eq 1 ]
+        then
+            # power
+            for i in $(seq $PORTS)
+            do
+                power_val=`cat /proc/power/active_pwr$((i))`
+                power_val=`printf "%.1f" $power_val`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/power -m "$power_val" -r
+            done
+        fi
+
+        if [ $energy -eq 1 ]
+        then
+            # energy consumption
+            for i in $(seq $PORTS)
+            do
+                energy_val=`cat /proc/power/cf_count$((i))`
+                energy_val=$(awk -vn1="$energy_val" -vn2="0.3125" 'BEGIN{print n1*n2}')
+                energy_val=`printf "%.0f" $energy_val`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/energy -m "$energy_val" -r
+            done
+        fi
+
+        if [ $voltage -eq 1 ]
+        then
+            # voltage
+            for i in $(seq $PORTS)
+            do
+                voltage_val=`cat /proc/power/v_rms$((i))`
+                voltage_val=`printf "%.1f" $voltage_val`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/voltage -m "$voltage_val" -r
+            done
+        fi
+
+        if [ $current -eq 1 ]
+        then
+            # current
+            for i in $(seq $PORTS)
+            do
+                current_val=`cat /proc/power/i_rms$((i))`
+                current_val=`printf "%.1f" $current_val`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/current -m "$current_val" -r
+            done
+        fi
+
+        if [ $lock -eq 1 ]
+        then
+            # lock
+            for i in $(seq $PORTS)
+            do
+                port_val=`cat /proc/power/lock$((i))`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/lock -m "$port_val" -r
+            done
+        fi
+
+        if [ $pf -eq 1 ]
+        then
+            # pf
+            for i in $(seq $PORTS)
+            do
+                pf_val=`cat /proc/power/pf$((i))`
+                pf_val=`printf "%.2f" $pf_val`
+                $PUBBIN -h $mqtthost $auth -t $topic/port$i/pf -m "$pf_val" -r
+            done
         fi
     fi
 
-fi
-
-if [ "$mFiType" == "mPower" ] || [ "$mFiType" == "mPower Mini" ] || [ "$mFiType" == "mPower Pro" ]
-then
-
-    log "Gathering mPower values."
-    if [ $relay -eq 1 ]
+    if [ $stat -eq 1 ]
     then
-        # relay state
-        for i in $(seq $PORTS)
-        do
-            relay_val=`cat /proc/power/relay$((i))`
-            if [ $relay_val -ne 1 ]
-            then
-              relay_val=0
-            fi
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/relay -m "$relay_val" -r
-        done
+      if [ $SLOWUPDATECOUNTER -le 0 ]
+      then
+          LOAD1=`awk '{print $1}' /proc/loadavg`
+          LOAD5=`awk '{print $2}' /proc/loadavg`
+          LOAD15=`awk '{print $3}' /proc/loadavg`
+          $PUBBIN -h $mqtthost $auth -t $topic/\$stats/load1 -m "$LOAD1" -r
+          $PUBBIN -h $mqtthost $auth -t $topic/\$stats/load5 -m "$LOAD5" -r
+          $PUBBIN -h $mqtthost $auth -t $topic/\$stats/load15 -m "$LOAD15" -r
+
+          UPTIME=`awk '{print $1}' /proc/uptime`
+          $PUBBIN -h $mqtthost $auth -t $topic/\$stats/uptime -m "$UPTIME" -r
+          SLOWUPDATECOUNTER=$((SLOWUPDATENUMBER))
+      else
+          SLOWUPDATECOUNTER=$((SLOWUPDATECOUNTER-1))
+      fi
     fi
 
-    if [ $power -eq 1 ]
-    then
-        # power
-        for i in $(seq $PORTS)
-        do
-            power_val=`cat /proc/power/active_pwr$((i))`
-            power_val=`printf "%.1f" $power_val`
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/power -m "$power_val" -r
-        done
-    fi
-
-    if [ $energy -eq 1 ]
-    then
-        # energy consumption
-        for i in $(seq $PORTS)
-        do
-            energy_val=`cat /proc/power/cf_count$((i))`
-            energy_val=$(awk -vn1="$energy_val" -vn2="0.3125" 'BEGIN{print n1*n2}')
-            energy_val=`printf "%.0f" $energy_val`
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/energy -m "$energy_val" -r
-        done
-    fi
-
-    if [ $voltage -eq 1 ]
-    then
-        # voltage
-        for i in $(seq $PORTS)
-        do
-            voltage_val=`cat /proc/power/v_rms$((i))`
-            voltage_val=`printf "%.1f" $voltage_val`
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/voltage -m "$voltage_val" -r
-        done
-    fi
-
-    if [ $current -eq 1 ]
-    then
-        # current
-        for i in $(seq $PORTS)
-        do
-            current_val=`cat /proc/power/i_rms$((i))`
-            current_val=`printf "%.1f" $current_val`
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/current -m "$current_val" -r
-        done
-    fi
-
-    if [ $lock -eq 1 ]
-    then
-        # lock
-        for i in $(seq $PORTS)
-        do
-            port_val=`cat /proc/power/lock$((i))`
-            $PUBBIN -h $mqtthost $auth -t $topic/port$i/lock -m "$port_val" -r
-        done
-    fi
-
-fi
 done

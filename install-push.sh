c085e764e1bf3ef1ff353295dada62e5ebf6c00c
MMV <mmv.rus@gmail.com>
2021-07-04 20:31:15 +0300

    Add usage hint to install-push.sh

--- install-push.sh
+++ install-push.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
+if [ "$1" == "" ] ; then
+    echo "Usage: install-push.sh <user>@<mfi-dev>"
+    exit
+fi
+
 # mFi devices use old weak auth method so enable it
 SSHOPT="-oKexAlgorithms=+diffie-hellman-group1-sha1"
 # Use persistent master connection to not enter password multiple times
